40651b59a04bd8e25d02edec4502be2488b93696
tayfundagci <tayfundagci3517@gmail.com>
2022-03-09 18:06:56 +0300

    filters add

--- 3-todo-app/src/components/TodoList.jsx
+++ 3-todo-app/src/components/TodoList.jsx
@@ -3,19 +3,29 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggle, destroy } from "../redux/todos/todosSlice";
 
+let filtered = [];
+
 function TodoList() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
 
   const handleDestroy = (id) => {
-    if (window.confirm("Are you sure?")) {
-      dispatch(destroy(id));
-    }
+    dispatch(destroy(id));
   };
+
+  filtered = items;
+  if (activeFilter !== "all") {
+    filtered = items.filter((todo) =>
+      activeFilter === "active"
+        ? todo.completed === false
+        : todo.completed === true
+    );
+  }
 
   return (
     <ul className="todo-list">
-      {items.map((item) => (
+      {filtered.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
             <input
